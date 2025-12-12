07b083541829040b7b681cfa923f357b42dc2904
Michael Carey <Michael@AGameEmpowerment.com>
2025-12-12 16:02:55 -0700

    ApiInfo updated

--- src/Blazor.Chat.App/Blazor.Chat.App.ApiService/ApiInfo.cs
+++ src/Blazor.Chat.App/Blazor.Chat.App.ApiService/ApiInfo.cs
@@ -1,5 +1,3 @@
-using Microsoft.OpenApi;
-
 namespace Blazor.Chat.App.ApiService;
 
 /// <summary>
@@ -7,21 +5,6 @@ namespace Blazor.Chat.App.ApiService;
 /// </summary>
 internal class ApiInfo
 {
-    /// <summary>
-    /// </summary>
-    /// <param name="version"></param>
-    /// <returns></returns>
-    public OpenApiInfo GetApiVersion(string version)
-    {
-        return new OpenApiInfo
-        {
-            Title = $"Chat API Service {version}",
-            Version = $"{version}",
-            Description = $"Chat API Service documentation, &copy; 2023 - {DateTime.UtcNow:yyyy} - Chat API Service - " +
-                          $"Build Version: {GetType().Assembly.GetName().Version}"
-        };
-    }
-
     public Version? GetAssemblyVersion()
     {
         return GetType().Assembly.GetName().Version;
